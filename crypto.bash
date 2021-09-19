f274578add4a1a89bccf65a137146398cd11c5f6
Daniel La Laina <daniel.lalaina@movile.com>
2021-09-19 20:40:42 +0000

    auto restart container

--- crypto.bash
+++ crypto.bash
@@ -6,7 +6,7 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 cd $script_dir
 
 if echo "$@" |grep -Eq  "overview.*[0-9]"; then
- docker run -d cryptobash "$@"
+ docker run -d --restart unless-stopped cryptobash "$@"
 else
  docker run cryptobash "$@"
 fi
