41219c457b2b658adbee79d1f9a0c5fb3253c422
Daniel La Laina <daniel.lalaina@movile.com>
2021-09-20 19:13:22 +0000

    auto build first time
    And handling old images

--- crypto.bash
+++ crypto.bash
@@ -5,9 +5,26 @@
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 cd $script_dir
 
-if echo "$@" |grep -Eq  "overview.*telegram.*[0-9]"; then
- docker run -d --restart unless-stopped cryptobash "$@"
+latest() {
+ latest=$(docker image ls |grep cryptobash |head -n1 |awk '{print $2}')
+}
+
+parameters="$@"
+
+if echo "$parameters" |grep -Eq  "stop"; then
+ for container in `docker ps |grep cryptobash |awk '{print $1}' 2>/dev/null`; do docker stop $container; done
+ for image in `docker image ls |grep cryptobash |awk '{print $3}' |tail -n+2 2>/dev/null`; do docker rmi -f $image; done
+ exit
+fi
+
+if [ -z $latest ]; then
+ docker build -t cryptobash:cryptobash_$RANDOM .
+ latest
+fi
+
+if echo "$parameters" |grep -Eq  "overview.*telegram.*[0-9]"; then
+ docker run -d --restart unless-stopped cryptobash:$latest $parameters
 else
- docker run -i cryptobash "$@"
+ docker run -i cryptobash:$latest $parameters
 fi
 
